c7bb13f8a0cae8d5aca644cc08f24649056bd762
Kravets_Dmytro <ditur@ukr.net>
2023-04-24 12:22:39 +0300

    + css

---
+++ src/fetchGallery.js
@@ -0,0 +1,45 @@
+import axios from 'axios';
+
+const API_KEY = '35566012-73194189fcef75c31768f8916';
+const URL = `https://pixabay.com/api/`;
+
+export class ApiFalleryImg {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+    this.perPage = 10;
+  }
+  async fetchGallery() {
+    const param = {
+      params: {
+        key: API_KEY,
+        q: this.searchQuery,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: 'true',
+        per_page: this.perPage,
+        page: this.page,
+      },
+    };
+    try {
+      const response = await axios.get(URL, param);
+      this.incrementPage();
+      return response.data;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  incrementPage() {
+    this.page = this.page + 1;
+  }
+  resetPage() {
+    this.page = 1;
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+}
